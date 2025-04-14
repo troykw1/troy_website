ad3ef87100d3a01972dbc748c3e67ad16188d9db
troykw1 <81531893+troykw1@users.noreply.github.com>
2025-04-13 17:31:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const app = express();              //Instantiate an express app, the main work 
 const path = require('path');
 const port = process.env.PORT || 3000;                  //Save the port number where your server will be listening
 
-//Idiomatic expression in express to route and respond to a client request
-app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-                                                        //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
-});
-
+// Serve static files from the "Contact" directory
 app.use(express.static(path.join(__dirname, 'Contact')));
 
-   app.get('/', (req, res) => {
-     res.sendFile(path.join(__dirname, 'Contact/index.html'));
-   });
+// Handle requests to the contact route and serve index.html from Contact folder
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, 'Contact/index.html'));
+});
 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`);
