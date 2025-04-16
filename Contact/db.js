1f28e22962d28c121272997e166ceac8fe122841
troykw1 <81531893+troykw1@users.noreply.github.com>
2025-04-16 14:03:33 -0700

    Update db.js

--- Contact/db.js
+++ Contact/db.js
@@ -2,7 +2,6 @@
 const { Client } = require('pg');
 const client = new Client({
     user: 'test_56z1_user',
-  //  host: 'oregon-postgres.render.com',
     host: 'dpg-cvu4fqre5dus73cg00lg-a.oregon-postgres.render.com'
     database: 'test_56z1',
     password: 'pxzCPED8b8aCc8JXJl4mDcUigBLt8RwY',
