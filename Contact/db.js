63c5970e11f36954a3f2e6d81d434ac876481aa9
troykw1 <81531893+troykw1@users.noreply.github.com>
2025-04-16 11:51:52 -0700

    Update db.js

--- Contact/db.js
+++ Contact/db.js
@@ -2,8 +2,7 @@
 const { Client } = require('pg');
 const client = new Client({
     user: 'test_56z1_user',
-  //  host: 'oregon-postgres.render.com',
-    host: 'dpg-cvu4fqre5dus73cg00lg-a',
+    host: 'oregon-postgres.render.com',
     database: 'test_56z1',
     password: 'pxzCPED8b8aCc8JXJl4mDcUigBLt8RwY',
     port: 5432, // Default PostgreSQL port
