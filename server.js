5430c60c4e0a8f9ef576d1280f8f092b1d136ddd
troykw1 <81531893+troykw1@users.noreply.github.com>
2026-03-01 12:48:21 -0800

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();              // Instantiate an express app
 const port = 3000;                  // Save the port number where your server will be listening
 
 // Middleware to parse form data
-app.use(express.json());
+// app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Serve static files from the current directory
