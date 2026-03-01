00fef729192c784eda7062be85f0367b2fb4ef09
troykw1 <81531893+troykw1@users.noreply.github.com>
2026-03-01 10:31:28 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const path = require('path');       // Added this to test 
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 3000;                  //Save the port number where your server will be listening
 
+const Crudapp = require('./Crudapp');
+const Contact = require('./Contact');
+const Csvtopostgres = require('./Csvtopostgres');
+
+console.log(Crudapp.message);
+console.log(Contact.message);
+console.log(Csvtopostgres.message);
+
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-  //   res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
-     res.sendFile(path.join(__dirname, 'index.html')); 
+res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
