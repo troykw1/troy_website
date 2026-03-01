6413cff8a99914ea95f2b3bc48a594828cc0f86b
troykw1 <81531893+troykw1@users.noreply.github.com>
2026-03-01 10:28:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-
+const path = require('path');       // Added this to test 
 const express = require('express'); //Import the express dependency
 const app = express();              //Instantiate an express app, the main work horse of this server
 const port = 3000;                  //Save the port number where your server will be listening
 
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
-    res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+  //   res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
+     res.sendFile(path.join(__dirname, 'index.html')); 
                                                         //the .sendFile method needs the absolute path to the file, see: https://expressjs.com/en/4x/api.html#res.sendFile 
 });
 
