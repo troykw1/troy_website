7781e644cd1858c2e787f7f5654cc643bcfa6b98
troykw1 <81531893+troykw1@users.noreply.github.com>
2026-03-01 12:18:26 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));  
 // app.use(express.static(path.join(__dirname, 'public')));
 
-// Route for the root URL
+// Home Page 
 app.get('/', (req, res) => {
-res.sendFile('index.html', { root: __dirname });
+    res.send('Welcome to Troy K. Wille's website, I hope you enjoy it. <br><br> I built my website with: <ul><a href='https://nodejs.org/docs/latest/api/'</a><li>Node.js</li><a href='https://en.wikipedia.org/wiki/REST'</a><li>REST API</li><a href='https://www.postgresql.org/'</a><li>PostgreSQL</li></ul>');
 });
+    
+// Route for the root URL
+// app.get('/', (req, res) => {                          commented out on 03/01/26 at 12:17 
+// res.sendFile('index.html', { root: __dirname });         commented out on 03/01/26 at 12:17 
+// });                                                            commented out on 03/01/26 at 12:17 
 
 // Route to handle form submission
 app.post('/submit', (req, res) => {
